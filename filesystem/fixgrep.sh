4f4b4e7acb67c7f6cbda5e4d629c9a98f484b5db
Jeremy Edgell <edgellj@gmail.com>
2010-04-04 11:57:18 -0400

    changed fixgrep.sh to use getopts in stead of trying to remember order of operators

--- filesystem/fixgrep.sh
+++ filesystem/fixgrep.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-find $1 -type $2 | xargs grep $3
+finda=
+findd=.
+
+while getopts 'd:t:a:p:' OPTS
+
+	do
+		case $OPTS in
+			d)	findd="$OPTARG" ;;
+			t)	findt="$OPTARG" ;;
+			a)	finda="$OPTARG" ;;
+			p)	grepp="$OPTARG" ;;
+			?)	echo 'Usage fixgrep.sh [-d base directory] [-t filetype] [-a other find args, eg --prune] [-p phrase]' 
+				exit 2 ;;
+		esac
+	done
+shift $(($OPTIND + 1))
+
+if [ ! $findt ]; then echo "Must include file type [-t]"; exit 2; fi
+if [ ! $grepp ]; then echo "Must include search string [-p]"; exit 2; fi
+
+if [ $finda ]; then
+	find "$findd" -type "$findt" "$finda"  | xargs grep "$grepp"
+else
+	find "$findd" -type "$findt" | xargs grep "$grepp"
+fi
